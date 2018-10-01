98a8bf1b59b9147490751b7107155f98aa6fadd5
Alex Fraser <afraser@thoughtworks.com>
2018-10-01 23:03:34 +1000

    Implemented MIDI rasterisation: time, pitch, variable, track

--- mugen/input.py
+++ mugen/input.py
@@ -1,35 +1,72 @@
 from math import ceil
 
-from mido import MidiFile
+import mido
 import numpy as np
 
 
-def load_midi(filepath):
-    return MidiFile(filepath)
+MIDI_MAX_VELOCITY = 127
 
 
-def rasterize(
-        mid: MidiFile, ticks_per_second: float = 12, polyphony: int = 24
-        ) -> np.array:
-    image = create_empty_image(
-        mid.length, ticks_per_second, polyphony)
-    quantize(mid, image)
-    return image
+class MIDIRasterizer:
+    def __init__(self, pitches: int = 64, pitch_offset: int = 32):
+        '''
+        Encodes MIDI data as a piano roll image with dimensions:
+        - step: regular absolute time (not variable offset as in MIDI)
+        - pitch: MIDI pitch e.g. keys on a keyboard
+        - data: various information of a note e.g. velocity
+        - track: the instrument
+        '''
+        self.steps_per_second = 6
+        self.pitches = pitches
+        self.pitch_offset = pitch_offset
 
+    def load_midi(self, filepath: str) -> mido.MidiFile:
+        return mido.MidiFile(filepath)
 
-def create_empty_image(
-        length: float, ticks_per_second: float, polyphony: int
-        ) -> np.array:
-    total_ticks = int(ceil(length * ticks_per_second))
+    def rasterize(self, mid: mido.MidiFile) -> np.array:
+        image = self.create_empty_image(mid.length)
+        self.initialize_time_series(image)
+        self.insert_messages(mid, image)
+        self.propagate_events_forward(image)
+        return image
 
-    # Concatenation of three one-hot vectors
-    n_voices = 128
-    n_pitches = 128
-    n_velocities = 128
-    data_vector_length = n_voices + n_pitches + n_velocities
+    def create_empty_image(self, length_s: float) -> np.array:
+        n_steps = int(ceil(length_s * self.steps_per_second))
+        width = self.pitches
+        n_variables = len(['velocity'])
+        n_tracks = 1
+        return np.empty((n_steps, width, n_variables, n_tracks))
 
-    return np.zeros(shape=(total_ticks, polyphony, data_vector_length))
+    def initialize_time_series(self, image):
+        # Set first time step to zero, to allow values to be propagated.
+        image[:] = np.nan
+        image[0] = 0
 
+    def insert_messages(self, mid: mido.MidiFile, image: np.array):
+        # Default nominal tempo is 120bpm
+        ticks_per_step = self.get_ticks_per_step(120, mid.ticks_per_beat)
 
-def quantize(mid: MidiFile, image: np.array):
-    pass
+        for track_i, track in enumerate(mid.tracks):
+            track_img = image[:, :, :, track_i]
+            current_tick = 0
+            for msg in track:
+                current_tick += msg.time
+                current_step = int(current_tick // ticks_per_step)
+                if msg.type == 'note_on':
+                    pitch = msg.note - self.pitch_offset
+                    track_img[current_step, pitch, 0] = (
+                        msg.velocity / MIDI_MAX_VELOCITY)
+                elif msg.type == 'note_off':
+                    pitch = msg.note - self.pitch_offset
+                    track_img[current_step, pitch, 0] = 0.0
+
+    def get_ticks_per_step(self, bpm, ticks_per_beat):
+        tempo = mido.bpm2tempo(bpm) / 1000000
+        s_per_step = 1 / self.steps_per_second
+        s_per_tick = tempo / ticks_per_beat
+        return s_per_step / s_per_tick
+
+    def propagate_events_forward(self, image: np.array):
+        for step_a, step_b in zip(image, image[1:]):
+            mask = np.isnan(step_b)
+            step_b[mask] = step_a[mask]
--- tests/test_input.py
+++ tests/test_input.py
@@ -1,6 +1,86 @@
+from mido import Message, MidiFile, MidiTrack
+import numpy as np
+from pytest import fixture
+
 from mugen import input
 
 
-def test_that_input_shape_is_as_expected():
-    image = input.create_empty_image(10, 12, 3)
-    assert image.shape == (10 * 12, 3, 128 * 3)
+@fixture
+def midifile():
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+
+    track.append(Message('program_change', program=12, time=0))
+    track.append(Message('note_on', note=32, velocity=64, time=160))
+    track.append(Message('note_on', note=33, velocity=32, time=0))
+    track.append(Message('note_off', note=32, velocity=127, time=160))
+    track.append(Message('note_on', note=33, velocity=0, time=160))
+
+    return mid
+
+
+@fixture
+def rasterizer():
+    return input.MIDIRasterizer(pitches=32, pitch_offset=32)
+
+
+def test_that_input_shape_is_as_expected(rasterizer):
+    image = rasterizer.create_empty_image(2.0)
+    steps = rasterizer.steps_per_second * 2
+    pitches = rasterizer.pitches
+    variables = 1
+    tracks = 1
+    assert steps == 12
+    assert pitches == 32
+    assert image.shape == (steps, pitches, variables, tracks)
+
+
+def test_that_an_initialized_image_is_all_nan_except_for_the_first_timestep(
+        rasterizer):
+    image = np.empty((4, 4, 1, 1))
+    rasterizer.initialize_time_series(image)
+    assert np.count_nonzero(np.isnan(image)) == 3 * 4
+    assert np.count_nonzero(image[0] == 0) == 4
+
+
+def test_that_note_messages_affect_one_pixel(
+        midifile, rasterizer, mocker):
+    rasterizer.get_ticks_per_step = mocker.MagicMock(return_value=160.0)
+    image = np.full((4, 32, 1, 1), np.nan)
+    rasterizer.insert_messages(midifile, image)
+
+    expected = np.full((4, 32, 1, 1), np.nan)
+    expected[:, :2, 0, 0] = np.array([
+        [np.nan, np.nan],
+        [64/127, 32/127],
+        [0,      np.nan],
+        [np.nan, 0],
+    ])
+    assert np.allclose(image, expected, equal_nan=True)
+
+
+def test_get_ticks_per_step(rasterizer):
+    assert rasterizer.steps_per_second == 6
+    assert rasterizer.get_ticks_per_step(120, 480) == 160
+
+
+def test_that_events_are_propagated(rasterizer):
+    image = np.array([
+        [0,      1,      0],
+        [np.nan, np.nan, np.nan],
+        [np.nan, np.nan, 1],
+        [np.nan, 0,      np.nan],
+        [1,      np.nan, np.nan],
+    ])
+    image.resize((5, 3, 1, 1))
+    rasterizer.propagate_events_forward(image)
+    expected = np.array([
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1],
+        [1, 0, 1],
+    ])
+    expected.resize((5, 3, 1, 1))
+    assert np.all(image == expected)
